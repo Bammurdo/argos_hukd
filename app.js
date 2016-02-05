15537d77b18cb3020c11c8c08eb49da568e002ab
Brandon Murdoch <bmurdoch1994@gmail.com>
2016-02-05 23:06:50 +0000

    Second api (fake_api) built to compare prices Now, it is a website, that calls the HUKD api, and sends the prices to a second api. The second api then comes up with it's own prices for the products. Then, the prices are compared, differences calculated, and they are sent to the display, where they are displayed(or not, if the difference is less than 0). Also, added a .gitignore - which I forgot at the start D: - huge node-modules got uploaded by accident :/
    Intructions on how to use my app to follow...

--- app.js
+++ app.js
@@ -1,28 +1,88 @@
 var express = require('express');
-var app = express();
 var request = require('request');
+var bodyParser = require('body-parser');
+var app = express();
 
-var port = 8080;
+var port = 8080;  // port for app
 
 app.set('view engine', 'jade');
 app.locals.pretty = true; // for pretty html ^-^
 app.use(express.static(__dirname + '/public')); // allows js (jquery/api_caller) to be passed directly
+// parse application/json
+app.use(bodyParser.json());
 
 // routes
 app.get('/', function(req, res) {
+  // when you just go to http://localhost:8080
+  // we just need to render the webpage
   res.render('home.jade');
 });
 app.post('/', function(req, res) {
-  // user wants the top ten!
+  // user has sent a post request, which we are just going to assume is for
+  // the top 10 lastest deals at Argos
+
+  // first, get deals from HUKDs
+  callHUKDapi(function(hukd_deals){
+    // pass the deals to the fake api to get some 'competition' prices
+    callFakeApi(hukd_deals, function(fake_api_deals){
+      // Get price difference between fake_api & argos
+
+      var hukd_items = hukd_deals.deals.items;  // skip straight to the items array
+      var fapi_items = fake_api_deals.items;
+      for(var i=0;i<hukd_items.length;i++){
+        var diff = 0;
+        // Don't do maths with decimals in javascript ... especially when talking about money
+        var hukPrice = hukd_items[i].price;  // HUKD price, as string
+        if (!(isNaN(hukPrice) || hukPrice === null)){
+          // if the hotukdeals is a valid price - find out the difference
+          hukPrice = Math.floor(parseFloat(hukPrice) * 100);
+          fpiPrice = Math.floor(parseFloat(fapi_items[i].ourPrice) * 100);
+          diff = fpiPrice - hukPrice;
+          var diff = fpiPrice - hukPrice;
+        }
+        hukd_items[i].diffInPrice = diff / 100;
+      }
+      // send all the deal information, along with the price differences to the website
+      res.send(hukd_deals);
+    });
+  });
+
+});
+
+function callHUKDapi(callback){
   var api_host = "http://api.hotukdeals.com";
   var api_path = "/rest_api/v2/";
   var api_path_opt = "?key=120e5aac30f8e92a6b33d858fdbdeb66&merchant=argos&results_per_page=10&page=1&output=json";
-  request(api_host + api_path + api_path_opt, function (err, api_res, body) {
-    if (!err && api_res.statusCode == 200) {
-      res.send(JSON.parse(body));
+  request(api_host + api_path + api_path_opt, function (err, response, body) {
+    if (!err && response.statusCode == 200) {
+      // send JSON of deals to our callback
+      callback(JSON.parse(body));
+      // hukd returns an object in the form:
+      // {'deals': items[{~deal information~}]}
     }
   });
-});
+}
+
+function callFakeApi(hukd_deals, callback){
+  // hukd_deals is a json obj that has a lot of information the fake api doesn't need
+  var hukd_items = hukd_deals.deals.items;  // skip to the items array
+  var fake_api_req = { "items":[]};
+  // Lets build the request to our fake api
+  // for each item in the deal list...
+  for(var i=0;i<hukd_items.length;i++){
+    fake_api_req.items[i] = {'product_name': hukd_items[i].title, 'price': hukd_items[i].price};
+  }
+
+  request({
+    url: 'http://localhost:8181',
+    method: 'POST',
+    json: fake_api_req
+  }, function (err, status, body) {
+    if (!err && status.statusCode == 200) {
+      callback(body);
+    }
+  });
+}
 
 app.listen(port, function() {
   console.log('Server started on port: ' + port);
--- public/js/api_caller.js
+++ public/js/api_caller.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 	loadTopTen();			// Load top ten once document is ready
+
 	$("#loadTopTen").click(function(e){
+		$('#deals').empty();
 		loadTopTen();		// reload, when button is clicked
 	});
 });
@@ -13,10 +15,9 @@ function loadTopTen(){
 		url: "/",
 		data: post_data,
 		success: function(data){
-			console.log("req was successful")
+			console.log("req was successful");
 		},
 		error: function(e){
-			alert('failed to reach server!');
 			console.log(e.message);
 		}
 	}).done(function(data){
@@ -27,29 +28,49 @@ function loadTopTen(){
 function displayTopTen(theDeals){
 	// render our items into the #deals list
 	$('#deals').empty();
+	// for each deal
 	for (var i=0;i<10;i++){
 		var temp = theDeals.deals.items[i].temperature;
 		temp = Math.round(temp);	// rounded temperature, because it looks much nicer
 		var price = theDeals.deals.items[i].price;
+		if (price === null) price = "-.--";	// hukd sometimes has null as the price...
 		var url_deal = theDeals.deals.items[i].deal_link;
 		var image_url = theDeals.deals.items[i].deal_image;
 		// "http://static.hotukdeals.com/images/threads/2388148_1.jpg"
-		// First 44 are always the same & remove the file type (any length)
+		// First 44chars are always the same & remove the file type (any length)
 		var image_id = image_url.substring(44).split('.')[0];	// hacky way to get image id
+		var price_diff = theDeals.deals.items[i].diffInPrice;
 		var argos_link = "http://www.hotukdeals.com/visit?m=5&q=" + image_id;
 		var desc = theDeals.deals.items[i].description;
 
-		$('#deals').append(
-			'<li class="ind_deal">' +
-				'<span class="temp">' + temp + '&deg;</span>' +
-				'<span class="price">&pound;' + price + '</span>' +
-				'<div class="deal_wrap">' +
-					'<img class="product_image" src="' + image_url +  '"/>' +
-					'<p class="desc">' + desc.substr(0, 50) + ' <a class="more" href="#more">more...</a></p>' +
-					'<a class="argos_link" href="' + argos_link + '">Argos</a>' +
-				'</div>' +
-			'</li>'
-		);
+		// If it is cheaper at argos, flaaunt it
+		if (parseFloat(price_diff) > 0){
+			$('#deals').append(
+				'<li class="ind_deal">' +
+					'<span class="temp">' + temp + '&deg;</span>' +
+					'<span class="price">&pound;' + price + '</span>' +
+					'<div class="deal_wrap">' +
+						'<img class="product_image" src="' + image_url +  '"/>' +
+						'<span class="price_diff" title="only at Argos!">&pound;' + price_diff.toFixed(2) + ' cheaper</span>' +
+						'<p class="desc">' + desc.substr(0, 50) + ' <a class="more" href="#more">more...</a></p>' +
+						'<a class="argos_link" href="' + argos_link + '">Argos</a>' +
+					'</div>' +
+				'</li>'
+			);
+		}else{
+			// if not, just keep quiet
+			$('#deals').append(
+				'<li class="ind_deal">' +
+					'<span class="temp">' + temp + '&deg;</span>' +
+					'<span class="price">&pound;' + price + '</span>' +
+					'<div class="deal_wrap">' +
+						'<img class="product_image" src="' + image_url +  '"/>' +
+						'<p class="desc">' + desc.substr(0, 50) + ' <a class="more" href="#more">more...</a></p>' +
+						'<a class="argos_link" href="' + argos_link + '">Argos</a>' +
+					'</div>' +
+				'</li>'
+			);
+		}
 	}
-	console.log(theDeals);
+	console.log(theDeals);	//debugg
 }
